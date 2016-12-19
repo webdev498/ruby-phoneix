4d7a143364b631207bc1d633e5a467db1c2dc915
webdev498 <gcmobile@mail.com>
2016-12-19 17:49:31 +0800

    Added wings attributes as permitted params to allow them update and change.

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -14,6 +14,7 @@ class FacilitiesController < ApplicationController
   # GET /facilities/1
   # GET /facilities/1.json
   def show
+    @facility.wings.build
     render :edit
   end
 
@@ -24,6 +25,7 @@ class FacilitiesController < ApplicationController
 
   # GET /facilities/1/edit
   def edit
+
   end
 
   # POST /facilities
@@ -82,6 +84,13 @@ class FacilitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def facility_params
-      params.require(:facility).permit(:dept_number, :facility_id_number, :name, :short_name, :active_flag, :address1, :address2, :city, :state, :zip_code, :account_number, :internal_id_number, :state_id_number, :npi_number, :price_schedule, :universal_fee, :unit_dose_fee, :control_drug_fee, :narcotic_fee, :allow_customer_discount, :label_type, :spool_labels, :label_default, :use_expiration_date, :expiration_default, :use_lot_number, :use_doc_u_dose, :default_to_primary_plan, :use_valid_division_codes, :use_form_flags, :use_start_date, :post_zero_copay, :use_auto_fill, :use_patient_counseling, :print_patient_counseling, :select_counseling, :check_dur, :print_monograph, :log_dur_results, :require_hippa_privacy_notice, :print_medication_administration_form, :print_physician_order_form, :print_treatment_form, :print_delivery_receipt, :medication_administration_form, :physician_orders_form, :treatment_form, :print_order, :print_pass_times, :print_other_allergy, :med_administration_routine_heading, :med_administration_prn_heading, :treatment_heading, :print_fill_date, :print_original_date, :print_in_frequency_order, :require_rx_copy_in_facility, :type_of_facility)
+      params.require(:facility).permit(:active, :id, :phone, :fax, :email, :facility_id_number, :name, :short_name, :active_flag, :address1, :address2, :city, :state, :zip_code, :account_number, :internal_id_number, :state_id_number, :npi_number, :price_schedule, :universal_fee, :unit_dose_fee, :control_drug_fee, :narcotic_fee, :allow_customer_discount, :label_type, :spool_labels, :label_default, :use_expiration_date, :expiration_default, :use_lot_number, :use_doc_u_dose, :default_to_primary_plan, :use_valid_division_codes, :use_form_flags, :use_start_date, :post_zero_copay, :use_auto_fill, :use_patient_counseling, :print_patient_counseling, :select_counseling, :check_dur, :print_monograph, :log_dur_results, :require_hippa_privacy_notice, :print_medication_administration_form, :print_physician_order_form, :print_treatment_form, :print_delivery_receipt, :medication_administration_form, :physician_orders_form, :treatment_form, :print_order, :print_pass_times, :print_other_allergy, :med_administration_routine_heading, :med_administration_prn_heading, :treatment_heading, :print_fill_date, :print_original_date, :print_in_frequency_order, :require_rx_copy_in_facility, :type_of_facility,
+      wings_attributes:[:id, :company_id,:pharmacy_id,:facility,:pass_times_id,:active,:name,:contact,:price_schedules,:universal_fee,:unit_dose_fee,:control_drug_fee \
+      ,:narcotic_fee,:allow_customer_discount,:label_type,:spool_labels,:label_default,:expiration_date,:expiration_default,:lot_number,:doc_u_dose\
+      ,:default_to_primary_plan,:valid_division_codes,:form_flags,:start_date,:post_zero_copay,:frequency_auto_fill,:anniversary_auto_fill,:procycle_auto_fill\
+      ,:print_monograph,:log_dur_results,:require_hippa_privacy_notice,:print_medication_guide,:print_medication_administration_form,:print_physician_order_form,:print_treatment_form\
+      ,:print_delivery_receipt,:medication_administration_form,:physician_orders_form,:treatment_form,:print_order,:print_pass_times,:print_other_allergy,:med_administration_routine_heading\
+      ,:med_administration_prn_heading,:treatment_heading,:print_fill_date,:print_original_date,:print_in_frequency_order,:require_rx_copy_in_facility,:expand_sig_codes,:standing_orders\
+      ,:type_of_facility,:emr_interface,:emr_interface_type])
     end
 end
