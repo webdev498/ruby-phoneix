2b44c0127febedc83fac229917eed3ecd49e9bae
webdev498 <gcmobile@mail.com>
2016-12-21 01:38:19 +0800

    Migrate new bed schema.

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,6 +1,6 @@
 class Bed < ActiveRecord::Base
   belongs_to :facility
   belongs_to :wing
-  belongs_to :room
   belongs_to :residency
+  belongs_to :customer
 end
--- spec/factories/beds.rb
+++ spec/factories/beds.rb
@@ -4,12 +4,14 @@ FactoryGirl.define do
 pharmacy_id 1
 facility nil
 wing nil
-room nil
 residency nil
+customer nil
+legacy_customer_id_number 1
 active false
+pass_order 1
 bed "MyString"
 bed_type "MyString"
-occupancy_date "2016-10-13"
+occupancy_date "2016-12-21"
   end
 
 end
